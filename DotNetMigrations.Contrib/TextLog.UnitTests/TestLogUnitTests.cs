44c7afd0d7de3143ad6f761b84e2c8d228901b9c
Joshua Poehls <joshua@zduck.com>
2010-05-15 11:06:55 -0500

    Initial Source Code Check-In (from CodePlex change set 39797 by JamesEggers

---
+++ DotNetMigrations.Contrib/TextLog.UnitTests/TestLogUnitTests.cs
@@ -0,0 +1,102 @@
+﻿using System.Configuration;
+using System.IO;
+using NUnit.Framework;
+
+namespace DotNetMigrations.Contrib.Logs.UnitTests_TextLog
+{
+    [TestFixture]
+    public class TestLogUnitTests
+    {
+        private string _logPath;
+
+        [TestFixtureSetUp]
+        public void SetupFixture()
+        {
+            _logPath = ConfigurationManager.AppSettings["logFilePath"];
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            if (File.Exists(_logPath))
+            {
+                File.Delete(_logPath);
+            }
+        }
+
+        [TestFixtureTearDown]
+        public void TearDownFixture()
+        {
+            if (File.Exists(_logPath))
+            {
+                File.Delete(_logPath);
+            }
+        }
+
+        [Test]
+        public void Should_Create_The_Log_File_Upon_Instantiation()
+        {
+            using (var log = new TextLog())
+            {
+                // do nothing.
+            }
+
+            Assert.IsTrue(File.Exists(_logPath));
+        }
+
+        [Test]
+        public void Should_Log_A_Message()
+        {
+            string message = "This is a test.";
+            
+            using (var log = new TextLog())
+            {
+                log.WriteLine(message);
+            }
+
+            Assert.IsTrue(ReadFile().Contains(message));
+        }
+
+        [Test]
+        public void Should_Log_A_Warning_Message()
+        {
+            string message = "This is a warning.";
+
+            using (var log = new TextLog())
+            {
+                log.WriteWarning(message);
+            }
+
+            string actual = ReadFile();
+            Assert.IsTrue(actual.Contains(message));
+            Assert.IsTrue(actual.Contains("__WARNING__"));
+        }
+
+        [Test]
+        public void Should_Log_An_Error_Message()
+        {
+            string message = "This is a error.";
+
+            using (var log = new TextLog())
+            {
+                log.WriteError(message);
+            }
+
+            string actual = ReadFile();
+            Assert.IsTrue(actual.Contains(message));
+            Assert.IsTrue(actual.Contains("__ERROR__"));
+        }
+
+        private string ReadFile()
+        {
+            string contents = null;
+
+            if (File.Exists(_logPath))
+            {
+                contents = File.ReadAllText(_logPath);
+            }
+            
+            return contents;
+        }
+    }
+}
---
+++ DotNetMigrations.Contrib/TextLog/TextLog.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using DotNetMigrations.Core;
+
+namespace DotNetMigrations.Contrib.Logs
+{
+    public class TextLog : LoggerBase
+    {
+        private StreamWriter _stream;
+        private const string _logName = "TextLog";
+        private string _padding = string.Empty.PadLeft(4);
+
+        /// <summary>
+        /// The name of the Log
+        /// </summary>
+        public override string LogName { get; set;}
+
+        /// <summary>
+        /// Instantiates a new instance of the TextLog class
+        /// </summary>
+        public TextLog()
+        {
+            LogName = _logName;
+            Initialize();
+        }
+        
+        /// <summary>
+        /// Writes an error message to the textual log file.
+        /// </summary>
+        /// <param name="message">The error message to log.</param>
+        public override void WriteError(string message)
+        {
+            _stream.WriteLine(DateTime.Now.ToString());
+            _stream.WriteLine(_padding + "__ERROR__");
+            _stream.WriteLine(_padding + message);
+            _stream.WriteLine(string.Empty);
+        }
+
+        /// <summary>
+        /// Write a message to the textual log file.
+        /// </summary>
+        /// <param name="message">The message to log.</param>
+        public override void WriteLine(string message)
+        {
+            _stream.WriteLine(DateTime.Now.ToString());
+            _stream.WriteLine(_padding + message);
+            _stream.WriteLine(string.Empty);
+        }
+
+        /// <summary>
+        /// Writes a warning message to the textual log file.
+        /// </summary>
+        /// <param name="message">The warning message to log.</param>
+        public override void WriteWarning(string message)
+        {
+            _stream.WriteLine(DateTime.Now.ToString());
+            _stream.WriteLine(_padding + "__WARNING__");
+            _stream.WriteLine(_padding + message);
+            _stream.WriteLine(string.Empty);
+        }
+
+        /// <summary>
+        /// Disposes of resources that were holding memory and other resources.
+        /// </summary>
+        public override void Dispose()
+        {
+            base.Dispose();
+
+            if (_stream != null)
+            {
+                _stream.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Initializes the logfile to write to.
+        /// </summary>
+        private void Initialize()
+        {
+            if (_stream == null)
+            {
+                string filePath = GetLogFilePath();
+                _stream = File.CreateText(filePath);
+            }
+        }
+
+        /// <summary>
+        /// Obtains the Log File path from the configuration file's AppSettings section
+        /// </summary>
+        /// <returns>The file path from the Configuration File's AppSettings 'logFilePath' entry.</returns>
+        private string GetLogFilePath()
+        {
+            return ConfigurationManager.AppSettings["logFilePath"];
+        }
+    }
+}
